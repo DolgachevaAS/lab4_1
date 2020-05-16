fc33c4ffc6be6d97cad3476891ff9e503a1deaad
DolgachevaAS <62135525+DolgachevaAS@users.noreply.github.com>
2020-05-16 17:57:14 +0300

    Add files via upload

---
+++ histogram1.cpp
@@ -0,0 +1,16 @@
+#include"histogram.h"
+#include<vector>
+#include <iostream>
+#include"histogram1.h"
+using namespace std;
+
+
+
+size_t procent( size_t number_count, size_t bin)
+{ size_t  proc=0;
+if (number_count=!0)
+{
+       proc=(bin*100)/number_count;
+}
+    return proc;
+}
---
+++ histogram1.h
@@ -0,0 +1,9 @@
+#ifndef HISTOGRAM_H_INCLUDED
+#define HISTOGRAM_H_INCLUDED
+
+#include<vector>
+using namespace std;
+
+
+size_t procent(size_t number_count,size_t bin);
+#endif // HISTOGRAM_H_INCLUDED
